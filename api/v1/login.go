477175e6ba626419490c631ab060218e9fe770d1
alexclownfish <ywz0207@163.com>
2022-08-24 14:14:41 +0800

    update 202208241414login resp message

--- api/v1/login.go
+++ api/v1/login.go
@@ -20,7 +20,7 @@ func Login(ctx *gin.Context) {
 		token, _ = middleware.SetToken(data.Username)
 	}
 	ctx.JSON(http.StatusOK, gin.H{
-		"loginName": xxx,
+		"loginName": "xxx",
 		"status":    code,
 		"message":   errmsg.GetErrMsg(code),
 		"token":     token,
